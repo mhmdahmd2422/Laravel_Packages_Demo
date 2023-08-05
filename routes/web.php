619fc69ca2d08ab166ceaaab33e3ee6a44d74110
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2023-08-06 00:46:41 +0300

    Demo of Saptie Laravel Permissions & Roles

--- routes/web.php
+++ routes/web.php
@@ -4,25 +4,29 @@ use App\Helpers\ImageFilter;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Intervention\Image\ImageManagerStatic as Image;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//////////////////////////////// YAJRA
+Route::get('/dashboard', function (\App\DataTables\UsersDataTable $dataTable) {
+    return $dataTable->render('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('user/{id}/edit', function ($id){
     return $id;
 })->name('user.edit');
+
+///////////////////////////////// Intervention
 
 Route::get('image', function (){
     $img = Image::make('Laravel-9.jpg')
@@ -31,7 +35,7 @@ Route::get('image', function (){
     return $img->response();
 });
 
-///////////////////////////
+/////////////////////////// Laravel Carts
 
 Route::get('shop', [\App\Http\Controllers\CartController::class, 'shop'])->name('shop');
 
@@ -47,16 +51,39 @@ Route::get('qty-increment/{rowId}', [\App\Http\Controllers\CartController::class
 
 Route::get('qty-decrement/{rowId}', [\App\Http\Controllers\CartController::class, 'qtyDecrement'])->name('qty-decrement');
 
-/////////////////////////////
+///////////////////////////// Spatie
 
-Route::get('/dashboard', function (\App\DataTables\UsersDataTable $dataTable) {
-    return $dataTable->render('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('create-role', function (){
+//   $role = Role::create(['name' => 'publisher']);
+//
+//   return $role;
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//    $permission = Permission::create(['name' => 'edit articles']);
+//
+//    return $permission;
+
+    $user = auth()->user();
+//    $user->assignRole('writer');
+//    $user->givePermissionTo('delete articles');
+
+//    return $user->getPermissionNames();
+//    return $user->getRoleNames();
+    $checkPermission = $user->can('delete articles');
+    if($checkPermission){
+        return 'user have delete articles permission';
+    }else{
+        return 'user doesnt have delete articles permission';
+    }
 });
+
+Route::get('posts', function (){
+    $posts = \App\Models\Post::all();
+
+    return view('post.post', compact('posts'));
+//    auth()->user()->assignRole('admin');
+});
+
+
+
 
 require __DIR__.'/auth.php';
